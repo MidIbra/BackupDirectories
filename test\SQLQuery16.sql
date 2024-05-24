0f64e76bf3879e27234c4f1048fd2f996033262f
MidIbra <ibraimimidjan@gmail.com>
2024-05-24 11:43:35 +0200

    Commit automatico dal programma C#

---
+++ test\SQLQuery16.sql
@@ -0,0 +1,8 @@
+CREATE TABLE TestTab(
+    ID_P int,
+    Nome varchar(255),
+ Cognome varchar(255),
+DataNasc DATE,
+
+    PRIMARY KEY(ID_P)
+)
---
+++ test\SQLQuery6.sql
@@ -0,0 +1,7 @@
+CREATE TABLE TabTest(
+ID_Persona int,
+Cognome varchar(255),
+Nome varchar(255),
+Indirizzo varchar(255),
+Citt� varchar(255)
+);
