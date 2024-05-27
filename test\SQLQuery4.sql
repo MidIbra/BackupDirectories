1c0a887fb8ff7c99d014cc4789e8b1d98ef6e5f2
MidIbra <ibraimimidjan@gmail.com>
2024-05-27 10:17:32 +0200

    Delete test\SQLQuery4.sql

--- test\SQLQuery4.sql
+++
@@ -1,5 +0,0 @@
-SELECT NomeCliente, Indirizzo, Nazione, DataOrdine
-FROM ((Clienti
-FULL JOIN Ordini ON Clienti.ID_Cliente=Ordini.ID_Cliente))
-WHERE NomeCliente LIKE 'a%' AND Nazione='Messico'
-ORDER BY Indirizzo DESC
